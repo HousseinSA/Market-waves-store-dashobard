39b7c0ba7e3c6498085f66e46d9d30a77a567b72
HousseinSA <HousseinSA>
2023-10-01 18:47:07 +0000

    Pushed from Locofy Builder

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import '../app/global.css'
+import "./global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ import RecentActivity from "../components/recent-activity";
 const Desktop = () => {
   return (
     <div className="relative bg-ghostwhite-100 w-full flex flex-col items-center justify-center p-[3.25rem] box-border md:flex md:p-[1.88rem] md:box-border sm:p-[0rem] sm:box-border">
-      <div className="self-stretch rounded-[25px] bg-grayscale-white flex flex-row items-center justify-center py-[0rem] px-[1.25rem] gap-[1.88rem] border-[10px] border-solid border-grayscale-white sm:rounded-none sm:h-screen">
+      <div className="self-stretch rounded-[25px] bg-grayscale-white flex flex-row items-center justify-center py-[0rem] px-[1.25rem] gap-[1.88rem] border-[10px] border-solid border-grayscale-white sm:rounded-none sm:p-[0rem] sm:box-border">
         <div className="flex flex-col items-center justify-center p-[0.63rem] md:hidden md:w-auto md:[align-self:unset]">
           <Sidenav />
         </div>
-        <div className="flex-1 overflow-hidden flex flex-col items-center justify-center py-[1.56rem] px-[1.25rem] gap-[1.25rem]">
+        <div className="flex-1 overflow-hidden flex flex-col items-center justify-center py-[1.56rem] px-[1.25rem] gap-[1.25rem] sm:py-[0.63rem] sm:px-[0rem] sm:box-border">
           <Header />
           <Buttons />
           <Inbox />
